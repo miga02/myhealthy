539831f57e284bb28049b60f59c878b97dfc601a
lyy <1244115215@qq.com>
2019-03-13 10:53:01 +0800

    first

---
+++ pages/welcome/welcome.js
@@ -0,0 +1,21 @@
+
+
+Page({
+  data: {
+ 
+  },
+  onLoad: function (a) {
+   
+  },
+  onReady: function () { },
+  onShow: function () {
+  
+  },
+  onHide: function () { },
+  onUnload: function () { },
+  onPullDownRefresh: function () { },
+  onReachBottom: function () { },
+  onShareAppMessage: function () { },
+
+ 
+});
